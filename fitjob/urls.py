163eed93dca07b9c4ca06dde80aec70524eeffd9
kqe123 <tnstn1155@naver.com>
2026-02-21 03:51:48 +0900

    로고 추가, 취업톡 추가

--- fitjob/urls.py
+++ fitjob/urls.py
@@ -3,5 +3,6 @@ from .views import *
 
 app_name = "fitjob"   # ✅ 이거 꼭 있어야 namespace가 생김
 urlpatterns = [
-    path("", main, name="main")
+    path("", main, name="main"),
+    path("board/", board, name="board"),
 ]
--- fitjob/views.py
+++ fitjob/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 
 def main(request) :
     return render(request, "fitjob/main.html")
+
+def board(request) :
+    return render(request, "fitjob/board.html")
 # Create your views here.
