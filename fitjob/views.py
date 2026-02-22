3cc1fb9c4a171080c7069ce56cd9a4deaf2ecd22
kqe123 <tnstn1155@naver.com>
2026-02-23 05:18:35 +0900

    취업톡 페이징 처리 완료

--- fitjob/views.py
+++ fitjob/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
-
+from .models import Question
+from django.core.paginator import Paginator
 def main(request) :
     return render(request, "fitjob/main.html")
 
 def board(request) :
-    return render(request, "fitjob/board.html")
-# Create your views here.
+    page = request.GET.get("page", 1) # 페이지 번호 (기본값: 1)
+    
+    questions = Question.objects.all().order_by("-created_at") # 질문 전체 가져옴 (최신순)
+
+    paginator = Paginator(questions, 10) # 페이지당 10개씩 보여줌.
+    page_obj = paginator.get_page(page) # 해당 페이지 객체 가져옴.
+    context = {"questions" : page_obj}
+    return render(request, "fitjob/board.html", context)
+
